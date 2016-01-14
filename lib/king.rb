7bf59a8bc6fca8b36d49042e4148da7dc856336c
Karin Fernandez <karinfdez@yahoo.com>
2016-01-14 18:49:00 -0500

    add distance comments to king

--- lib/king.rb
+++ lib/king.rb
@@ -6,6 +6,9 @@ class King
 	end
 
 	def move?(destx,desty)
+		# dx = (destx - @postx).abs
+		# dy = (desty - @posty).abs
+		
 		(desty == @posty || desty == @posty + 1 || desty == @posty - 1) &&
 		(destx == @postx || destx == @postx + 1 || destx == @postx - 1)
 	end
